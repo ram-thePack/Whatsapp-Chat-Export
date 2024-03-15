3f6bcec21e4754ed77beeb6e92f8db6b2309cb43
ram-thePack <162415223+ram-thePack@users.noreply.github.com>
2024-03-15 13:03:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// var http = require('http');
-
-// //create a server object:
-// http
-//   .createServer(function (req, res) {
-//     res.write('Hello World'); //write a response to the client
-//     res.end(); //end the response
-//   })
-//   .listen(80); //the server object listens on port 80
-
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const express = require('express');
@@ -18,11 +8,10 @@ app.listen(port, () => {
   console.log(`Server listening on the port ${port}`);
 });
 
-const AllSessionsObject = {};
-//const client = new Client();
 const client = new Client({
   puppeteer: {
-    headless: false, //make it as true when running on AWS EC2
+    headless: true,
+    args: ['--no-sandbox','--disable-gpu'],
   },
   authStrategy: new LocalAuth({
     clientId: 'YOUR_CLIENT_ID',
@@ -43,11 +32,8 @@ client.on('ready', () => {
 
 client.on('message', async (msg) => {
   let chat = await msg.getChat();
-  if (chat.isGroup) {
-    const date = new Date(msg.timestamp * 1000)
-      .toISOString()
-      .replace(/T/, ' ')
-      .replace(/\..+/, '');
+  if (chat.isGroup && chat.name.toLowerCase().includes("pack")) {
+    const date = new Date(msg.timestamp * 1000).toLocaleString();
     console.log(chat.name);
     console.log(msg.author.substring(0, msg.author.length - 5));
     console.log(date);
