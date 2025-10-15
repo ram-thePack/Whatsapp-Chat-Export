0d4103c3b3030e6731fd2639be4b4223524fd746
ram-thePack <tech@thepack.in>
2025-10-15 13:03:45 +0530

    Added KAi to all the groups

--- index.js
+++ index.js
@@ -189,10 +189,8 @@ client.on('message', async (msg) => {
 
   //Quert to KAi
   if (
-    chat.name.toLowerCase() == 'testing pack' ||
     chat.name.toLowerCase() == 'community moderation' ||
-    chat.name.toLowerCase() == 'thepack.in alpha' ||
-    chat.name.toLowerCase().includes('shih tzu parents pack')
+    chat.name.toLowerCase().includes('pack')
   ) {
     if (msg.body.toLowerCase().includes('@askkai')) {
       console.log('Query to KAi received, Query= ' + msg.body);
